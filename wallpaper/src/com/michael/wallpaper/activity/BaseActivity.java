028831e86da43fd57bcbc26c9605b4184536c3ff
guoqing.sun <sunguoqing@xunlei.com>
2014-05-06 12:37:58 +0800

    init

---
+++ wallpaper/src/com/michael/wallpaper/activity/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.michael.wallpaper.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by zhangdi on 14-3-5.
+ */
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+}
